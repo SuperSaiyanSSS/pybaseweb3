5ca9a5dd24c57edb359acee7c1d7cda5064b0ffc
Wei <1084441269@qq.com>
2024-11-05 16:27:59 +0800

    Update demo1.py

--- demo1.py
+++ demo1.py
@@ -4,3 +4,13 @@ from web3 import Web3
 infura_url = 'https://mainnet.infura.io/v3/YOUR_INFURA_PROJECT_ID'
 web3 = Web3(Web3.HTTPProvider(infura_url))
 print(web3.isConnected())
+# 合约的ABI和地址
+contract_abi = [...] 
+contract_address = '0xContractAddress'
+
+# 获取合约对象
+contract = web3.eth.contract(address=contract_address, abi=contract_abi)
+
+# 调用合约方法
+result = contract.functions.yourFunctionName().call()
+print(result)
