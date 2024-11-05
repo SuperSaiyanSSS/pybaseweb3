293a0ba95e56487ea5bb9849e504e155af5ec236
Wei <1084441269@qq.com>
2024-11-05 15:35:18 +0800

    Update demo1.py

--- demo1.py
+++ demo1.py
@@ -3,6 +3,4 @@ from web3 import Web3
 # 使用 Infura 的 HTTP 连接（以太坊主网）
 infura_url = 'https://mainnet.infura.io/v3/YOUR_INFURA_PROJECT_ID'
 web3 = Web3(Web3.HTTPProvider(infura_url))
-
-
 print(web3.isConnected())
